b9a98a846c156405938ed1d806da2fc2c5da5f0f
Sjoerd Houben <houben.sjoerd@gmail.com>
2014-05-09 18:13:40 +0200

    Initial Working Software

---
+++ SingleFolderAutomatedMover/FormMain.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SingleFolderAutomatedMover
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.listBoxLogging = new System.Windows.Forms.ListBox();
+            this.notifyIconMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(15, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(125, 62);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 77);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(40, 13);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "Status:";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(186, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(112, 62);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // listBoxLogging
+            // 
+            this.listBoxLogging.FormattingEnabled = true;
+            this.listBoxLogging.Location = new System.Drawing.Point(12, 93);
+            this.listBoxLogging.Name = "listBoxLogging";
+            this.listBoxLogging.ScrollAlwaysVisible = true;
+            this.listBoxLogging.Size = new System.Drawing.Size(881, 225);
+            this.listBoxLogging.TabIndex = 5;
+            // 
+            // notifyIconMain
+            // 
+            this.notifyIconMain.Text = "notifyIcon1";
+            this.notifyIconMain.Visible = true;
+            this.notifyIconMain.DoubleClick += new System.EventHandler(this.notifyIconMain_DoubleClick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 334);
+            this.Controls.Add(this.listBoxLogging);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.ListBox listBoxLogging;
+        private System.Windows.Forms.NotifyIcon notifyIconMain;
+    }
+}
+
